c0dc8d515df89ca9baab4028ac873eb1e2c7511b
Zubayer Rahaman <zubayer.skd@gmail.com>
2023-02-16 21:25:26 +0600

    added my old site

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# zubathecoder
-Zubayer Rahaman, the best programmer of bangladesh, is showcasing his profile! Let's go! Have a look: https://www.zubayer.one/
+"# zuba" 
---
+++ script.js
@@ -0,0 +1,43 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function dropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.ClassList.contains('show')) {
+        openDropdown.ClassList.remove('show');
+      }
+    }
+  }
+}
+
+function openNav() {
+	document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+}
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
